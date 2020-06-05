51405a90a117f2ead6e904722fbca1af61d1bda0
Santi <scros97@gmail.com>
2020-06-05 18:30:14 +0200

    Blog show content

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <h1>This is a blog post</h1>
+    <h1 class="text-2xl">{{ post.title }}</h1>
+    <p>{{ post.date }}</p>
+
+    <div class="mt-4">
+      <nuxt-content :document="post" />
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async fetch() {
+    this.post = await this.$content('blog', this.$route.params.slug)
+      .sortBy('date', 'desc')
+      .fetch()
+  },
+  data() {
+    return {
+      post: {}
+    }
+  }
+}
 </script>
 
-<style></style>
+<style scoped>
+.nuxt-content h1 {
+  @apply text-xl;
+}
+</style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -3,7 +3,7 @@
     <h1 class="text-4xl">Blog</h1>
     <ul>
       <li v-for="post in posts" :key="post.date">
-        <nuxt-link :to="'blog/' + post.slug">
+        <nuxt-link :to="'/blog/' + post.slug">
           <h2 class="text-3xl">{{ post.title }}</h2>
           <p>{{ post.description }}</p>
         </nuxt-link>
