dce7cc67e7928a29c9d47f351c014bcfc9115a3a
Santi <scros97@gmail.com>
2020-06-07 01:17:03 +0200

    Fix styles

--- components/contentBlocks/CallToAction.vue
+++ components/contentBlocks/CallToAction.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="call-to-action bg-gray-800 py-16">
+  <div class="call-to-action bg-gray-200 dark:bg-gray-800 py-16">
     <div class="px-6 max-w-3xl mx-auto">
       <h1 class="text-2xl">{{ block.header }}</h1>
       <p>{{ block.description }}</p>
       <div class="mt-4">
-        <button class="px-10 py-2 bg-gray-700 rounded">
+        <button class="px-10 py-2 bg-gray-300 dark:bg-gray-700 rounded">
           {{ block.button }}
         </button>
       </div>
